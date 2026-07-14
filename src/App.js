c4652980d9876c1b7afae8124b7e88e03b4262b3
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-07-14 20:39:34 -0300

    Feat: Cria o componente do Logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./componentes/Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Alura
-        </a>
+        <Logo/>
       </header>
+
     </div>
   );
 }
