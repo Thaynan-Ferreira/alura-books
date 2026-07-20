f56aa1cacb6df1977008f84c00cfda742347b186
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-07-20 20:28:11 -0300

    Refactor: Adiciona o Styled-component ao projeto

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,15 +1,20 @@
 import Logo from "../Logo";
 import IconesHeader from "../IconesHeader";
 import OpcoesHeader from "../OpceosHeader";
+import styled from "styled-components";
 
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;`
 
 function Header(){
     return (
-        <header className="App-header">
+        <HeaderContainer>
             <Logo/>
             <OpcoesHeader/>
             <IconesHeader/>
-        </header>
+        </HeaderContainer>
     )
 }
 
